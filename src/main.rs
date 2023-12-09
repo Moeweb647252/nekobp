786b029a477f5e3cef4806395ac99381c81ac33a
Misaka Natsuxun <misaka@nekoit.org>
2023-12-10 01:11:41 +0800

    Add bind config args

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 use futures_util::StreamExt;
 use log::{info, warn};
 use ntex::web;
+use std::io::{self, Write};
+
+fn input(prompt: &str) -> io::Result<String> {
+    io::stdout().write(prompt.as_bytes())?;
+    io::stdout().flush()?;
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf)?;
+    #[cfg(debug_assertions)]
+    info!("input: {}", buf);
+    Ok(buf)
+}
+
 async fn hello() -> &'static str {
     "Hello"
 }
@@ -48,9 +60,23 @@ async fn dl(path: web::types::Path<(String, String)>, req: web::HttpRequest) -> 
 }
 
 #[ntex::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> io::Result<()> {
     std::env::set_var("RUST_LOG", "info");
     env_logger::init();
+
+    let args: Vec<String> = std::env::args().collect();
+    let usage = format!("Usage: {} [addr] / [ip] [port]", args[0]);
+    let (addr, port) = match args.len() {
+        1 => (input("addr:")?, input("port:")?.parse().expect(&usage)),
+        2 => {
+            let mut args = args[1].split(":");
+            (
+                args.next().expect(&usage).to_string(),
+                args.next().expect(&usage).parse().expect(&usage),
+            )
+        }
+        _ => (args[1].clone(), args[2].parse().expect(&usage)),
+    };
 
     web::server(|| {
         web::App::new()
@@ -61,7 +87,7 @@ async fn main() -> std::io::Result<()> {
                 web::resource("/").to(hello),
             ))
     })
-    .bind("127.0.0.1:8080")?
+    .bind((addr, port))?
     .run()
     .await
 }
