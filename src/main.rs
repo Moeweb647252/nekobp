1d69c13d5ffcf1ca37903bacf195257e80364706
Misaka <misaka@nekoit.org>
2023-12-23 16:30:26 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use futures_util::StreamExt;
 use log::{info, warn};
 use ntex::{util::Bytes, web};
-use std::io::{self, Write};
+use std::{
+    convert::Infallible,
+    io::{self, Write},
+};
+
+const HOST: &str = "073.pw";
 
 fn input(prompt: &str) -> io::Result<String> {
     io::stdout().write(prompt.as_bytes())?;
@@ -13,28 +18,79 @@ fn input(prompt: &str) -> io::Result<String> {
     Ok(buf)
 }
 
-fn handel_resp(resp: reqwest::Response) -> web::HttpResponse {
-    let mut builder = web::HttpResponse::Ok();
-    builder.content_type(
-        resp.headers()
-            .get("Content-Type")
-            .map(|t| t.to_str().unwrap_or("application/octet-stream"))
-            .unwrap_or("application/octet-stream"),
-    );
-    if let Some(t) = resp.content_length() {
-        builder.content_length(t);
+trait HandelResp {
+    fn handel_stream_resp(resp: reqwest::Response) -> web::HttpResponse;
+    fn handel_resp(headers: reqwest::header::HeaderMap, content: Vec<u8>) -> web::HttpResponse;
+    fn is_stream() -> bool;
+}
+
+struct Zero;
+
+impl Iterator for Zero {
+    type Item = Result<ntex::util::Bytes, Infallible>;
+    fn next(&mut self) -> Option<Self::Item> {
+        Some(ntex::util::Bytes::try_from(vec![0 as u8; 1024]))
     }
-    let stream = resp
-        .bytes_stream()
-        .map(|s| s.map(|s| ntex::util::Bytes::from(s.to_vec())));
-    return builder.streaming(stream);
+}
+
+struct HandelDlResp;
+
+impl HandelResp for HandelDlResp {
+    fn handel_stream_resp(resp: reqwest::Response) -> web::HttpResponse {
+        let mut builder = web::HttpResponse::Ok();
+        resp.headers()
+            .iter()
+            .map(|i| (i.0.to_string(), i.1.to_str().unwrap_or("").to_string()))
+            .for_each(|i| {
+                builder.set_header(i.0, i.1);
+            });
+        let stream = resp
+            .bytes_stream()
+            .map(|s| s.map(|s| ntex::util::Bytes::from(s.to_vec())));
+        return builder.streaming(stream);
+    }
+    fn handel_resp(_: reqwest::header::HeaderMap, _: Vec<u8>) -> web::HttpResponse {
+        todo!()
+    }
+    fn is_stream() -> bool {
+        true
+    }
+}
+
+struct HandelMikananiResp;
+
+impl HandelResp for HandelMikananiResp {
+    fn handel_resp(headers: reqwest::header::HeaderMap, content: Vec<u8>) -> web::HttpResponse {
+        let mut builder = web::HttpResponse::Ok();
+        headers
+            .iter()
+            .map(|i| (i.0.to_string(), i.1.to_str().unwrap_or("").to_string()))
+            .for_each(|i| {
+                builder.set_header(i.0, i.1);
+            });
+        if let Ok(content) = String::from_utf8(content) {
+            builder.body(content.replace("https://", format!("https://{}/", HOST).as_str()))
+        } else {
+            builder.body("")
+        }
+    }
+    fn handel_stream_resp(_: reqwest::Response) -> web::HttpResponse {
+        todo!()
+    }
+    fn is_stream() -> bool {
+        false
+    }
 }
 
 async fn hello() -> &'static str {
     "Hello"
 }
 
-async fn dl(
+async fn zero() -> web::HttpResponse {
+    web::HttpResponse::Ok().streaming(futures_util::stream::iter(Zero))
+}
+
+async fn dl<T: HandelResp>(
     path: web::types::Path<(String, String)>,
     req: web::HttpRequest,
     body: Bytes,
@@ -48,6 +104,8 @@ async fn dl(
     }
     let host = &path.0;
     let path = &path.1;
+    #[cfg(debug_assertions)]
+    info!("host: {}, path:{}", host, path);
     let builder = reqwest::Client::builder();
     let client = match builder.build() {
         Ok(client) => client,
@@ -68,7 +126,20 @@ async fn dl(
     .send()
     .await
     {
-        Ok(resp) => return handel_resp(resp),
+        Ok(resp) => {
+            return match T::is_stream() {
+                true => T::handel_stream_resp(resp),
+
+                false => {
+                    let headers = resp.headers().clone();
+                    if let Ok(content) = resp.bytes().await {
+                        T::handel_resp(headers, content.to_vec())
+                    } else {
+                        web::HttpResponse::ServiceUnavailable().body("")
+                    }
+                }
+            }
+        }
         Err(_) => match match req.method().as_str() {
             "GET" => client.get(format!("http://{}/{}", host, path)),
             "POST" => client.post(format!("http://{}/{}", host, path)),
@@ -79,7 +150,20 @@ async fn dl(
         .send()
         .await
         {
-            Ok(resp) => return handel_resp(resp),
+            Ok(resp) => {
+                return match T::is_stream() {
+                    true => T::handel_stream_resp(resp),
+
+                    false => {
+                        let headers = resp.headers().clone();
+                        if let Ok(content) = resp.bytes().await {
+                            T::handel_resp(headers, content.to_vec())
+                        } else {
+                            web::HttpResponse::ServiceUnavailable().body("")
+                        }
+                    }
+                }
+            }
             Err(error) => {
                 warn!("Error making request to {}/{}: {}", host, path, error);
                 return web::HttpResponse::ServiceUnavailable().body("503");
@@ -115,12 +199,14 @@ async fn main() -> io::Result<()> {
             // enable logger
             .wrap(web::middleware::Logger::default())
             .service((
-                web::resource("{domain}/{path}*").to(dl),
-                web::resource("/").to(hello),
+                web::resource("rss/{domain}/{path}*").to(dl::<HandelMikananiResp>),
+                web::resource("{domain}/{path}*").to(dl::<HandelDlResp>),
+                web::resource("").to(hello),
+                web::resource("zero").to(zero),
             ))
     })
     .bind((addr, port))?
-    .workers(num_cpus::get())
+    .workers(num_cpus::get() * 2)
     .run()
     .await
 }
